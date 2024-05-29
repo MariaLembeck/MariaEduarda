3bd50bdc90d5876f25fef5798d50c1816257d0f6
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2024-05-29 14:25:15 -0300

    algumas alterações

--- backup/tela2.php
+++ backup/tela2.php
@@ -1,6 +1,8 @@
+
 <?php
 include "header.php";
 ?> 
+
 <header>
         <div class="search-container">
             <input type="text" name="search" placeholder="Busque seu destino..." class="search-input">
--- login.php
+++ login.php
@@ -1,52 +1,28 @@
 <?php
 include "header.php";
 ?>
-
-
 <div id="login">
-
-<form class="card">
-
-    <div class="card-header">
-
-        <h2>Login</h2>
-
-    </div>
-
-    <div class="card-content">
-
-        <div class="card-content-area">
-
-            <label for="usuario">Usuário</label>
-
-            <input type="text" id="usuario" autocomplete="off">
-
+    <form class="card">
+        <div class="card-header">
+            <h2>Login</h2>
         </div>
-
-        <div class="card-content-area">
-
-            <label for="password">Senha</label>
-
-            <input type="password" id="password" autocomplete="off">
-
+        <div class="card-content">
+            <div class="card-content-area">
+                <label for="usuario">Usuário</label>
+                <input type="text" id="usuario" autocomplete="off">
+            </div>
+            <div class="card-content-area">
+                <label for="password">Senha</label>
+                <input type="password" id="password" autocomplete="off">
+            </div>
         </div>
-
-    </div>
-
-    <div class="card-footer">
-
-        <input type="submit" value="login" class="submit">
-        <div class="fonte">
-            <a href="#" class="recuperar_senha">Esqueceu a senha?</a>
+        <div class="card-footer">
+            <input type="submit" value="login" class="submit">
+            <div class="fonte">
+                <a href="#" class="recuperar_senha">Esqueceu a senha?</a>
+            </div>
         </div>
-        
-
-    </div>
-
-</form>
-
+    </form>
 </div>
-
 </body>
-
 </html>
