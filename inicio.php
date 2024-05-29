1a934b15ad07c300925ac0620b8f132dba3a3fa1
Your Name <you@example.com>
2024-05-29 13:45:46 -0300

    SkyWay php

---
+++ inicio.php
@@ -0,0 +1,39 @@
+<?php
+include "header.php";
+?>
+
+<header>
+    <section>
+        
+        <div id="background-wrap">
+            <div class="x1">
+                <div class="cloud"></div>
+            </div>
+        
+            <div class="x2">
+                <div class="cloud"></div>
+            </div>
+        
+            <div class="x3">
+                <div class="cloud"></div>
+            </div>
+        
+            <div class="x4">
+                <div class="cloud"></div>
+            </div>
+        
+            <div class="x5">
+                <div class="cloud"></div>
+            </div>
+        </div>
+    </section>
+
+    <div class="Db">
+        <img src="logo.png" style="margin-bottom: 50px;">
+        <a href="tela2.php"> <button class="animated-button" >Ache seu caminho</button></a>
+    </div>
+</header>
+
+</body>
+</html>
+
---
+++ tela2.php
@@ -0,0 +1,36 @@
+
+<?php
+include "header.php";
+?> 
+
+<header>
+        <div class="search-container">
+            <input type="text" name="search" placeholder="Busque seu destino..." class="search-input">
+            <a href="#" class="search-btn">
+                    <i class="fas fa-search"></i>      
+            </a>
+        </div>
+    <div class="user-parent">
+        <div class="user-child">
+            <a href="login.php" style="text-decoration: none;" >
+               <h5>Login</h5>
+               <div class="background">
+                   <i class="fa-solid fa-user"></i>
+            </a>
+               </div>
+       </div>
+    </div>
+
+</header>
+
+<div class="paris">
+            <div class="paris-background">
+                <div class="paris-img">
+                    <img src="https://cdn.sortiraparis.com/images/80/104172/981129-le-musee-de-la-marine-a-paris-et-ses-collections-permanentes-tour-eiffel-trocadero.jpg">
+                </div>
+        </div>
+    </div>
+ 
+</body>
+</html>
+
